9454bf72e1916d54a6699447446509849ed55aba
Eugio <47667122+ejhalpin@users.noreply.github.com>
2019-07-10 14:38:17 -0400

    finished with some minor fixes needed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,11 @@
-$(document).ready(function() {
-  loadSlides();
-  playSlides();
+$(document).on("click", ".link", function() {
+  var link = $(this).attr("data-link");
+  if (link.includes("www")) {
+    window.open(link, "_blank");
+  } else if (link.includes(".html")) {
+    window.location.href = link;
+  } else {
+    var url = "https://ejhalpin.github.io/" + link + "/";
+    window.open(url, "_blank");
+  }
 });
